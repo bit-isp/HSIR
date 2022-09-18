89e793eb410a725421ee4f690f2624d540840040
zeqiang-lai <laizeqiang@outlook.com>
2022-09-18 20:39:00 +0800

    update command line entry

--- hsiboard/main.py
+++ hsiboard/main.py
@@ -1,21 +1,29 @@
-import streamlit as st
 import argparse
+import streamlit as st
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 import viewer
-import cmp
 import util
+import cmp
 
-page_names_to_funcs = {
-    "Viewer": viewer.main,
-    "Comparator": cmp.main,
-}
+def main(logdir):
+    page_names_to_funcs = {
+        "Viewer": viewer.main,
+        "Comparator": cmp.main,
+    }
 
-parser = argparse.ArgumentParser('HSIR Board')
-parser.add_argument('--logdir', default='results')
-args = parser.parse_args()
+    selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+
+    util.set_page_container_style()
+    page_names_to_funcs[selected_page](logdir)
 
 
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('HSIR Board')
+    parser.add_argument('--logdir', default='results')
+    args = parser.parse_args()
 
-util.set_page_container_style()
-page_names_to_funcs[selected_page](args.logdir)
+    main(args.logdir)
