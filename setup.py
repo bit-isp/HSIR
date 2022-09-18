a3eff0cbe0b1aa338d584559ae509e88476b1c69
zeqiang-lai <laizeqiang@outlook.com>
2022-09-18 22:18:51 +0800

    release 0.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     url='https://github.com/Zeqiang-Lai/HSIR',
     packages=packages,
     package_dir={'hsir': 'hsir', 'hsirrun':'hsirrun', 'hsiboard': 'hsiboard'},
-    version='0.0.1',
+    version='0.1',
     include_package_data=True,
-    install_requires=['tqdm', 'qqdm', 'timm'],
+    install_requires=['tqdm', 'qqdm', 'timm', 'streamlit'],
 )
